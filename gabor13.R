2902271f9bd3c4eb0fcd6626dd9887522354ee9a
Borysław Paulewicz <borys@localhost.localdomain>
2016-02-25 16:36:05 +0100

    Dodalem instrukcje do wypalania obrazu Win32DiskImagerem

--- gabor13.R
+++ gabor13.R
@@ -2,6 +2,10 @@
 
 ## Aktualna wersja obrazu systemu operacyjnego jest tutaj: http://149.156.92.47/image-R
 
+## Aby skopiować obraz systemu na pendrive'a pod Windowsem można użyć
+## programu Win32DiskImager, dostępnego tutaj:
+## https://sourceforge.net/projects/win32diskimager/
+
 ## TODO wielkość czcionki w oknie z instrukcją, w ogóle wygląd okna z instrukcją
 
 if(interactive())source('~/cs/code/r/tasks/task/task.R')
